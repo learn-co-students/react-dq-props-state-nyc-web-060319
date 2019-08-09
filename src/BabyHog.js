3b719fffd0bf54d534cf787fbc2e41b5019182cb
te68 <te68@cornell.edu>
2019-08-09 10:12:43 -0400

    Done.

--- src/BabyHog.js
+++ src/BabyHog.js
@@ -5,14 +5,20 @@ import SunBaby from './assets/sun-eyes.png'
 import BlueBaby from './assets/blue-eyes.png'
 import GlowingBaby from './assets/glowing-eyes.png'
 
-// is there a way we could associate eye color string values with images? 
+// is there a way we could associate eye color string values with images?
 // perhaps so we could do something along the lines of `eyeColorMapper['blue'] and get back the right image?`
 
 export default class BabyHog extends Component {
 
-  constructor(props) {
-    super(props)
-  }
+    state = {
+      weight: 0,
+      eyeColorMapper: {
+        blue: BlueBaby,
+        sun: SunBaby,
+        glowing: GlowingBaby
+      },
+      height: 200
+    }
 
   changeWeight = (e) => {
     // nothing needs to change here
@@ -22,25 +28,41 @@ export default class BabyHog extends Component {
     })
   }
 
+  changeSize = (e) => {
+    let newHeight = e.target.name === "+" ? (this.state.height + 10) : (this.state.height - 10)
+    this.setState({
+      height: newHeight
+    })
+  }
+
+
   render() {
+    const height = this.state.height + 'px'
+    const baby = this.props.offspring
     return (
       <li className="hogbabies">
-        <h1>Name</h1>
-        <h3>Weight:</h3>
-        <h3>Hobby:</h3>
-        <h4>Eye Color:</h4>
-          
-        <Button name="+">
+        <h1>Name: {baby.name}</h1>
+        <h3>Weight: {this.state.weight} </h3>
+        <h3>Hobby: {baby.hobby}</h3>
+        <h4>Eye Color: {this.props.eyeColor}</h4>
+
+        <Button onClick={(event) => {
+            this.changeWeight(event)
+            this.changeSize(event)
+          }} name="+">
           Increase Weight
         </Button>
-        <Button name="-">
+        <Button onClick={(event) => {
+            this.changeWeight(event)
+            this.changeSize(event)
+          }}name="-">
           Decrease Weight
         </Button>
 
         <div className="hb-wrap">
-          <img src={normalBaby} style={{height: '200px'}} alt="MasterBlasterJrJr" />
+          <img src={this.state.eyeColorMapper[this.props.eyeColor]} style={{height: height }} alt="MasterBlasterJrJr" />
         </div>
-        
+
       </li>
     )
   }
